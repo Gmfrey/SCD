9bb7579293adeba40359db403907f53f26c69ebe
Gmfrey <30850840+Gmfrey@users.noreply.github.com>
2018-10-06 10:10:16 -0400

    Workers Working
    Workewrs working.
    
    Next up is supply.

--- assets/scripts/mainbase.js
+++ assets/scripts/mainbase.js
@@ -2,76 +2,95 @@ var minerals = 1000;
 
 var mineralsEarned = 0;
 
-var mineralsPerSecond = 0;
-
 var gas = 0;
 
 var gasEarned = 0;
 
-var gasPerSecond = 0;
+var idleWorkers = 0;
 
-var idleworkers = 0;
+var mineralWorkers = 12;
 
-var mineralworkers = 12;
+var gasWorkers = 0;
 
-var gasworkers = 0;
+var mineralPatch = 8;
 
-var mineralpatch = 8;
+var gasPatch = 2;
 
-var gaspatch = 2;
+var time = 5000;
+
+var supply = 12;
+
+var supplyLimit = 200;
+
+
+
 
 //Window Loop
 window.setInterval(function(){
-    mineminerals();
-    minegas();
-}, 5000);
+    mineMinerals();
+    mineGas();
+    document.getElementById("time").innerHTML = time;
+}, time);
 
 
-//Worker Function - minerals
+//Create Idle Worker
 
 function createWorkers(){
-  if (minerals >= 50){
-  idleworkers = idleworkers + 1;
+  if (minerals >= 50 &&  supply < supplyLimit){
+  idleWorkers = idleWorkers + 1;
   minerals = minerals - 50;
+  supply = supply + 1;
 };
-  document.getElementById("idleworkers").innerHTML = idleworkers;
-  document.getElementById("mineralworkers").innerHTML = mineralworkers;
+  document.getElementById("idleWorkers").innerHTML = idleWorkers;
+  document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
   document.getElementById("minerals").innerHTML = minerals;
   document.getElementById("gas").innerHTML = gas;
+  document.getElementById("supply").innerHTML = supply;
+  document.getElementById("supplyLimit").innerHTML = supplyLimit;
 };
 
-//Assign workers - Minerals
-function assignworkermineral(){
-  if (idleworkers >= 1 && mineralworkers < mineralpatch){
-    idleworkers = idleworkers - 1;
-    mineralworkers = mineralworkers +1;
-    document.getElementById("idleworkers").innerHTML = idleworkers;
-    document.getElementById("mineralworkers").innerHTML = mineralworkers;
+//Assign workers - minerals
+function assignWorkerMineral(){
+  if (idleWorkers >= 1 && mineralWorkers < mineralPatch * 2){
+    idleWorkers = idleWorkers - 1;
+    mineralWorkers = mineralWorkers + 1;
+    document.getElementById("idleWorkers").innerHTML = idleWorkers;
+    document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
     document.getElementById("minerals").innerHTML = minerals;
     document.getElementById("gas").innerHTML = gas;
   }
 }
 //Assign workers - gas
-function assignworkergas(){
-  if (idleworkers >= 1){
-    idleworkers = idleworkers - 1;
-    gasworkers = gasworkers +1;
-    document.getElementById("idleworkers").innerHTML = idleworkers;
-    document.getElementById("gasworkers").innerHTML = gasworkers;
+function assignWorkerGas(){
+  if (idleWorkers >= 1 && gasWorkers < gasPatch * 3){
+    idleWorkers = idleWorkers - 1;
+    gasWorkers = gasWorkers + 1;
+    document.getElementById("idleWorkers").innerHTML = idleWorkers;
+    document.getElementById("gasWorkers").innerHTML = gasWorkers;
     document.getElementById("minerals").innerHTML = minerals;
     document.getElementById("gas").innerHTML = gas;
   }
 }
 
 //Mine minerals function
-function mineminerals(){
-  mineralsEarned = mineralworkers * 5;
+function mineMinerals(){
+  mineralsEarned = mineralWorkers * 5;
   minerals = minerals + mineralsEarned
   document.getElementById("minerals").innerHTML = minerals;
 }
 //Mine gas function
-function minegas(){
-  gasEarned = gasworkers * 5;
+function mineGas(){
+  gasEarned = gasWorkers * 5;
   gas = gas + gasEarned
   document.getElementById("gas").innerHTML = gas;
 }
+
+
+//Update All Values in HTML page
+document.getElementById("supply").innerHTML = supply;
+document.getElementById("supplyLimit").innerHTML = supplyLimit;
+document.getElementById("idleWorkers").innerHTML = idleWorkers;
+document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
+document.getElementById("gasWorkers").innerHTML = gasWorkers;
+document.getElementById("minerals").innerHTML = minerals;
+document.getElementById("gas").innerHTML = gas;
