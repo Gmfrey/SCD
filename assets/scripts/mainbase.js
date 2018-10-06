2352f1ba8e520841308c55503fe718ac4cfa7529
Gmfrey <30850840+Gmfrey@users.noreply.github.com>
2018-10-06 12:09:02 -0400

    Working Supply Counter
    Working Supply Counter

--- assets/scripts/mainbase.js
+++ assets/scripts/mainbase.js
@@ -1,4 +1,4 @@
-var minerals = 1000;
+var minerals = 0;
 
 var mineralsEarned = 0;
 
@@ -20,18 +20,65 @@ var time = 5000;
 
 var supply = 12;
 
-var supplyLimit = 200;
+var supplyLimit = 15;
+
+var commandCenter = 1;
+
+var orbitalCommand = 0;
+
+var planetaryFortress = 0;
+
+var supplyDepot = 0;
+
+var refinery = 0;
+
+var barracks = 0;
+
+var engineeringBay = 0;
+
+var bunker = 0;
+
+var missileTurret = 0;
+
+var sensorTower = 0;
+
+var factory = 0;
+
+var ghostAcademy = 0;
+
+var armory = 0;
+
+var starport = 0;
+
+var fusionCore = 0;
+
+var techLab = 0;
+
+var reactor = 0;
 
 
 
-
-//Window Loop
+//Window Loop Mining
 window.setInterval(function(){
     mineMinerals();
     mineGas();
+    calcSupply();
     document.getElementById("time").innerHTML = time;
 }, time);
 
+//Window loop update UI each second
+window.setInterval(function(){
+    updateValues();
+}, 1000);
+
+//Calculate Supply Availible
+function calcSupply(){
+  supplyLimit = 15*(commandCenter + orbitalCommand + planetaryFortress) + 8*supplyDepot;
+  if (supplyLimit > 200){
+    supplyLimit = 200;
+  }
+  document.getElementById("supplyLimit").innerHTML = supplyLimit;
+};
 
 //Create Idle Worker
 
@@ -58,8 +105,8 @@ function assignWorkerMineral(){
     document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
     document.getElementById("minerals").innerHTML = minerals;
     document.getElementById("gas").innerHTML = gas;
-  }
-}
+  };
+};
 //Assign workers - gas
 function assignWorkerGas(){
   if (idleWorkers >= 1 && gasWorkers < gasPatch * 3){
@@ -69,28 +116,40 @@ function assignWorkerGas(){
     document.getElementById("gasWorkers").innerHTML = gasWorkers;
     document.getElementById("minerals").innerHTML = minerals;
     document.getElementById("gas").innerHTML = gas;
-  }
-}
+  };
+};
 
 //Mine minerals function
 function mineMinerals(){
   mineralsEarned = mineralWorkers * 5;
   minerals = minerals + mineralsEarned
   document.getElementById("minerals").innerHTML = minerals;
-}
+};
 //Mine gas function
 function mineGas(){
   gasEarned = gasWorkers * 5;
   gas = gas + gasEarned
   document.getElementById("gas").innerHTML = gas;
-}
+};
+//Create Supply Depot
+
+function createsupplyDepot(){
+  if (minerals >= 100){
+  minerals = minerals - 100;
+  supplyDepot = supplyDepot + 1;
+  document.getElementById("supplyDepot").innerHTML = supplyDepot;
+  document.getElementById("minerals").innerHTML = minerals;
+};
 
 
-//Update All Values in HTML page
-document.getElementById("supply").innerHTML = supply;
-document.getElementById("supplyLimit").innerHTML = supplyLimit;
-document.getElementById("idleWorkers").innerHTML = idleWorkers;
-document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
-document.getElementById("gasWorkers").innerHTML = gasWorkers;
-document.getElementById("minerals").innerHTML = minerals;
-document.getElementById("gas").innerHTML = gas;
+//Update All Values in HTML page every second
+function updateValues(){
+  document.getElementById("supply").innerHTML = supply;
+  document.getElementById("supplyLimit").innerHTML = supplyLimit;
+  document.getElementById("idleWorkers").innerHTML = idleWorkers;
+  document.getElementById("mineralWorkers").innerHTML = mineralWorkers;
+  document.getElementById("gasWorkers").innerHTML = gasWorkers;
+  document.getElementById("minerals").innerHTML = minerals;
+  document.getElementById("gas").innerHTML = gas;
+  document.getElementById("supplyDepot").innerHTML = supplyDepot;
+};};
